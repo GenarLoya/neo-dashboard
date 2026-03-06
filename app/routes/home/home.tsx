176ad21d3b7c6ac194ca7cdc91e5d0ef2ea864af
ganrLoya <genarodour210@gmail.com>
2026-03-05 20:33:57 -0600

    fix: asteroids per day

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -2,14 +2,14 @@ import { getNeoGeneralMeasures } from "data/neo/getNeoGeneralMeasures.service";
 import { Suspense } from "react";
 import { Await } from "react-router";
 import type { Route } from "./+types/home";
-import { AsteroidsPerDay } from "./components/AsteroidsPerDay";
+import { AsteroidsPerDay } from "./components/AsteroidsPerDay/AsteroidsPerDay";
 import { getHomeSearchParams } from "./searchParams";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const searchParams = getHomeSearchParams(request.url);
 
   return {
-    getNeoGeneralMeasures: await getNeoGeneralMeasures(
+    getNeoGeneralMeasures: getNeoGeneralMeasures(
       searchParams.startDate,
       searchParams.endDate,
     ),
@@ -18,14 +18,16 @@ export async function loader({ request }: Route.LoaderArgs) {
 
 export default function Component({ loaderData }: Route.ComponentProps) {
   return (
-    <div>
+    <div className="flex flex-col gap-4 p-4">
       <Suspense fallback={<div>Loadings...</div>}>
         <Await resolve={loaderData.getNeoGeneralMeasures}>
           {(data) => (
-            <AsteroidsPerDay
-              asteroidsPerDay={data.asteroidsPerDay}
-              dateRange={data.dateRange}
-            ></AsteroidsPerDay>
+            <>
+              <AsteroidsPerDay
+                asteroidsPerDay={data.asteroidsPerDay}
+                dateRange={data.dateRange}
+              />
+            </>
           )}
         </Await>
       </Suspense>
